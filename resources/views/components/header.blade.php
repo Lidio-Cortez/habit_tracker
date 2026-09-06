3d361a071ebd6f5d1534cada06934c600509557a
Lidio Cortez <Lidio334Cortez@gmail.com>
2026-09-06 07:55:19 +0100

    feat: add habit history view

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,29 +1,31 @@
-<header class="bg-white flex items-center justify-between p-4 border-b-2">
-    <div class="flex items-center gap-2">
-        <a href="{{ route('habits.index') }}" class="habit-btn habit-shadow-lg px-2 py-1 bg-habit-orange">
-            HT
-        </a>
-        <p>
-            Habit Tracker
-        </p>
-    </div>
-    <div>
-
-        @auth
+<header class=" bg-white p-4 border-b-2">
+    <div class="max-w-7xl mx-auto flex items-center justify-between">
+        <div class="flex items-center gap-2 font-bold">
+            <a href="{{ route('habits.index') }}" class="habit-btn habit-shadow-lg px-2 py-1 bg-habit-orange">
+                HT
+            </a>
+            <p>
+                Habit Tracker
+            </p>
+        </div>
+        <div>
+            
+            @auth
             <a href="{{ route('auth.logout') }}" class="bg-white habit-shadow-lg habit-btn p-2 border-2 inline">
                 Sair
             </a>
-        @endauth
-
-        @guest
+            @endauth
+            
+            @guest
             <div class="flex gap-2">
                 <a href="{{ route('site.login') }}" class="bg-habit-orange habit-shadow-lg p-2 border-2">
-                Logar
-            </a>   
-             <a href="{{ route('site.register') }}" class=" habit-shadow-lg p-2 border-2">
-                Registar
-            </a>   
+                    Logar
+                </a>   
+                <a href="{{ route('site.register') }}" class=" habit-shadow-lg p-2 border-2">
+                    Registar
+                </a>   
             </div>
-        @endguest
+            @endguest
+        </div>
     </div>
 </header>
